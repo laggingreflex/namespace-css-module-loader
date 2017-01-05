43c39e068eabeb6e9023ae8ce93321c5fab5d554
laggingreflex <laggingreflex@gmail.com>
2017-01-05 21:53:16 +0530

    ignore rules beginning with :global

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var plugin = postcss.plugin(pkg.name, function(opts) {
   var id = opts.id || 'style'
   return function(root) {
     return root.walkRules(function(rule) {
+      if (rule.selector.substr(0, 7) === ':global') return;
       rule.selector = ':local(.' + id + ') ' + rule.selector;
     })
   }
